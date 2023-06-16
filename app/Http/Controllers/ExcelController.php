5baf07543346e61d561604ce33e2643a2e91b20e
Parsa-Div-R <parsabibinia@gmail.com>
2023-06-16 21:32:08 +0430

    reading excel

--- app/Http/Controllers/ExcelController.php
+++ app/Http/Controllers/ExcelController.php
@@ -3,12 +3,41 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+
+
 class ExcelController extends Controller
 {
 
     function readingFile()
     {
-        $className = \PhpOffice\PhpSpreadsheet\Writer\Pdf\Dompdf::class;
-        return dd($className);
+
+        $file = 'C:\Users\Parsa-Div-\Desktop\testxlsx.xlsx';
+        $spreadsheet = IOFactory::load($file);
+
+
+        // Get the active sheet
+        $worksheet = $spreadsheet->getActiveSheet();
+        // Get the value of a specific cell
+        $cellValue = $worksheet->getCell('B3')->getValue();
+        // Display the cell value
+        $dimandy1= array($cellValue);
+
+        $highestRow = $worksheet->getHighestRow();
+// Get the column data
+        $columnData = $worksheet->rangeToArray('B4:B' . $highestRow);
+// Remove empty cells
+        $columnData = array_filter($columnData);
+// Display the column data
+        array_push($dimandy1,$columnData);
+        foreach ($dimandy1 as $numbers) {
+            if (is_array($numbers)) {
+                foreach ($numbers as $number) {
+                   dd ( $number,$numbers);
+                }
+            } else {
+                dd($numbers) ;
+            }
+        }
     }
 }
